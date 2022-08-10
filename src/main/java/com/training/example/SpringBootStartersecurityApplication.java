0a38e07b6020f642837cb479705df0a244945503
rajashekharreddy <grreddyjava1@gmail.com>
2022-08-10 10:17:32 +0530

    One file added

---
+++ src/main/java/com/training/example/SpringBootStartersecurityApplication.java
@@ -0,0 +1,13 @@
+package com.training.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartersecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartersecurityApplication.class, args);
+	}
+
+}
